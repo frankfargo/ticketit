9d8060b8c648136b9464b5e2be76f58ff77867c7
liang xie <xieliang12@gmail.com>
2016-02-26 13:12:32 -0700

    Admins can now set a deffault state for tickets

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,11 @@ Rails.application.routes.draw do
   namespace :admin do
     get 'users/index'
 
-    resources :states, only: [:index, :new, :create]
+    resources :states, only: [:index, :new, :create] do
+      member do
+        get :make_default
+      end
+    end
   end
 
   namespace :admin do
