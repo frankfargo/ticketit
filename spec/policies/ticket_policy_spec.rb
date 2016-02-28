c451921977b8dc6a4ea233cccb728c41ae846d25
liang xie <xieliang12@gmail.com>
2016-02-27 23:10:27 -0700

    Restrict tagging to only authorized users

--- spec/policies/ticket_policy_spec.rb
+++ spec/policies/ticket_policy_spec.rb
@@ -16,6 +16,7 @@ RSpec.describe TicketPolicy do
       it { should_not permit_action :update }
       it { should_not permit_action :destroy }
       it { should_not permit_action :change_state }
+      it { should_not permit_action :tag }
     end
 
     context "for viewers of the project" do
@@ -26,6 +27,7 @@ RSpec.describe TicketPolicy do
       it { should_not permit_action :update }
       it { should_not permit_action :destroy }
       it { should_not permit_action :change_state }
+      it { should_not permit_action :tag }
     end
 
     context "for editors of the project" do
@@ -36,6 +38,7 @@ RSpec.describe TicketPolicy do
       it { should_not permit_action :update }
       it { should_not permit_action :destroy }
       it { should_not permit_action :change_state }
+      it { should_not permit_action :tag }
       context "when the editor created the ticket" do
         before { ticket.author = user }
 
@@ -51,6 +54,7 @@ RSpec.describe TicketPolicy do
       it { should permit_action :update }
       it { should permit_action :destroy }
       it { should permit_action :change_state }
+      it { should permit_action :tag }
     end
 
     context "for managers of other projects" do
@@ -63,6 +67,7 @@ RSpec.describe TicketPolicy do
       it { should_not permit_action :update }
       it { should_not permit_action :destroy }
       it { should_not permit_action :change_state }
+      it { should_not permit_action :tag }
     end
 
     context "for administrators" do
@@ -72,6 +77,7 @@ RSpec.describe TicketPolicy do
       it { should permit_action :update }
       it { should permit_action :destroy }
       it { should permit_action :change_state }
+      it { should permit_action :tag }
     end
   end
 end
